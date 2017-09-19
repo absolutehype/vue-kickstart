213af2eeefc0843e454541ffb5d57188bbbae979
Piero Maltese <maltesepiero@gmail.com>
2017-09-19 09:21:22 +0100

    moved registerTransport in the right file

--- template/src/vuex/orders/index.js
+++ template/src/vuex/orders/index.js
@@ -1,8 +1,6 @@
 import Vue from 'vue';
-import httpProviderInstance from '../httpProviderInstance';
 
 export default function () {
-	Vue.$ofsCrud.registerTransport({ name: 'http', fn: () => httpProviderInstance });
 	Vue.$ofsCrud.addResource({
 		name: 'order',
 		options: { namespaced: true }
--- template/src/vuex/setupModules.js
+++ template/src/vuex/setupModules.js
@@ -1,9 +1,11 @@
 import Vue from 'vue';
 import OfsCrud from '@oneflow/ofs-vuex-crud';
+import httpProviderInstance from './httpProviderInstance';
 import registerOrdersModule from './orders';
 
 const setupModules = store => {
 	Vue.use(OfsCrud, { store });
+	Vue.$ofsCrud.registerTransport({ name: 'http', fn: () => httpProviderInstance });
 	registerOrdersModule();
 };
 
